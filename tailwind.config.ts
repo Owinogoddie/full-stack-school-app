6310289f24e13a4a6a5aef31759f9275aa17b233
Godfrey Owino <112401055+Owinogoddie@users.noreply.github.com>
2024-09-26 15:39:23 +0000

    adding sidebar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,12 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        lamaSky: "#C3EBFA",
+        lamaSkyLight: "#EDF9FD",
+        lamaPurple: "#CFCEFF",
+        lamaPurpleLight: "#F1F0FF",
+        lamaYellow: "#FAE27C",
+        lamaYellowLight: "#FEFCE8",
       },
     },
   },
