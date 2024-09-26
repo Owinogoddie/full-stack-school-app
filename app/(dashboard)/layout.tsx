b23ec91a180cf2b442e7e6ce30dcc35a6f24b9e8
Owinogoddie <owinogoddie@gmail.com>
2024-09-26 12:27:09 +0000

    first commit

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const DashboardLayout = () => {
+  return (
+    <div className="h-full flex">
+        {/* LEFT */}
+        .W-[14%]
+    </div>
+  )
+}
+
+export default DashboardLayout
