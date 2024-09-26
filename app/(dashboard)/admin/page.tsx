65cd4bfc3cb90f2881b51aced4540901b59f4ac0
Godfrey Owino <112401055+Owinogoddie@users.noreply.github.com>
2024-09-26 16:18:17 +0000

    adding list

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import Announcements from "@/components/announcements";
+import AttendanceChart from "@/components/charts/attendance-chart";
+import CountChart from "@/components/charts/count-chart";
+import EventCalendar from "@/components/calendars/event-calendar";
+import FinanceChart from "@/components/charts/finance-chart";
+import UserCard from "@/components/user-card";
 
 const AdminPage = () => {
   return (
-    <div>AdminPage</div>
-  )
-}
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* USER CARDS */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+        {/* MIDDLE CHARTS */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* COUNT CHART */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />
+          </div>
+        </div>
+        {/* BOTTOM CHART */}
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements/>
+      </div>
+    </div>
+  );
+};
 
-export default AdminPage
+export default AdminPage;
--- app/(dashboard)/student/page.tsx
+++ app/(dashboard)/student/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import Announcements from "@/components/announcements";
+import BigCalendar from "@/components/calendars/big-calendar";
+import EventCalendar from "@/components/calendars/event-calendar";
 
-const StudentsPage = () => {
+const StudentPage = () => {
   return (
-    <div>StudentsPage</div>
-  )
-}
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          <BigCalendar/>
+        </div>
+      </div>
+      {/* RIGHT */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
+    </div>
+  );
+};
 
-export default StudentsPage
+export default StudentPage;
